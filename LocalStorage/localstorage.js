6e6b28ee490e996ef06db99f54931c5e8fead01b
leo8219 <91192756+leo8219@users.noreply.github.com>
2025-08-24 20:55:35 +0900

    설명내용추가

--- LocalStorage/localstorage.js
+++ LocalStorage/localstorage.js
@@ -8,6 +8,8 @@ console.log(userLoginInfo);
 
 userIdvalue.textContent = 'id : ${userLoginInfo.id}';
 userPwvalue.textContent = 'pw : ${userLoginInfo.pw}';
+//이렇게하면 콘솔에는 객체형태가 아닌 문자열이 출력되고 웹에는 undefined 출력됨.
+//로컬스토리지에서setitem사용해 id,pw값을 객체의 형태로 저장할 때, 이 객체를 json문자열로 변환했기 때문에 다시 이걸 객체형태로 되돌려야함. (JSON.pares()사용)
 
 loginbutton.addEventListener("click", ()=>{
     console.log(idvalue.value);
@@ -19,6 +21,7 @@ loginbutton.addEventListener("click", ()=>{
     let userinfo = {id: idvalue.value, pw: pwvalue.value}
     localStorage.setItem("userinfo",JSON.stringify(userinfo));
     //id와 pw를 userinfo라는 이름 하나로 묶어 두 값을 하나의 객체 형태로 저장
+	//로컬스토리지 이용해 데이터 저장할땐 객체형태나 배열 형태로 여러값들을 하나의 key로 저장함
 })
 //localstorage.html에서 id, pw를 입력하고 btn을 클릭하면 콘솔로그에 id, pw 출력됨
 
@@ -29,4 +32,4 @@ loginbutton.addEventListener("click", ()=>{
 //consol탭에서Application이동 후 localstorage눌러보면 저장되어있음.
 
 //로컬스토지에 객체(ex:userinfo)를 넣을 땐 JSON이라는 JS객체 표기법으로 객체를 JSON 문자열로 변환한 다음 저장해야 함.
-
+//JSON.stringify 사용해 객체를 JSON 문자열로 넣어주면 id, pw값이 키값으로 할당됨
